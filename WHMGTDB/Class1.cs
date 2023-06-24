56b9682da84a4d0531a08b3aef07347849a93f6e
zhichengzhao <60787822+zhichengzhao@users.noreply.github.com>
2023-06-25 01:00:29 +0800

    6.25

---
+++ WHMGTDB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHMGTDB
+{
+    public class Class1
+    {
+    }
+}
